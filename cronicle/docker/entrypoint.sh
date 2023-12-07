d472597f3fb407483e7f2a1adc287b0385303365
Attapon Thanawong <attapon_srem@hotmail.com>
2023-12-07 12:46:04 +0700

    Modified: .gitignore, cronicle/cronicle-stack.yaml, cronicle/docker/entrypoint.sh

--- cronicle/docker/entrypoint.sh
+++ cronicle/docker/entrypoint.sh
@@ -26,8 +26,9 @@ then
     # Move in custom configuration
     cp $DATA_DIR/config.json.import $CONF_DIR/config.json
   fi
-
-  $BIN_DIR/control.sh setup
+  if [ -z "$WORKER" ]; then
+    $BIN_DIR/control.sh setup
+  fi
 
   # Create plugins directory
   mkdir -p $PLUGINS_DIR
@@ -36,10 +37,6 @@ then
   touch $DATA_DIR/.setup_done
 fi
 
-# Run cronicle with unprivileged user
-# chown -R cronicle:cronicle data/ logs/
-
-# remove old lock file. resolves #9
 PID_FILE=$LOGS_DIR/cronicled.pid
 if [ -f "$PID_FILE" ]; then
     echo "Removing old PID file: $PID_FILE"
@@ -48,11 +45,7 @@ fi
 
 if [ -n "$1" ];
 then
-#   if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-#     set -- cronicle "$@"
-#   fi
   exec "$@"
 else
-#   exec su cronicle -c "/opt/cronicle/bin/control.sh start"
   /opt/cronicle/bin/control.sh start
 fi
